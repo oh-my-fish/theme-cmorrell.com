2b1940e56bf2166289ae5732e8c1682ab90aee31
Dmitry Tyschenko <rainydio@gmail.com>
2015-11-21 12:01:43 +0200

    Added mercurial support.

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,14 +1,13 @@
+function __cmorrell_show_vcs_status -d "Displays VCS status"
+  if [ (count $argv) = 2 ]
+    set -l dirty $argv[1]
+    set -l branch $argv[2]
 
-function get_git_status -d "Gets the current git status"
-  if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
-    set -l dirty (command git status -s --ignore-submodules=dirty | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
-    set -l ref (command git symbolic-ref --short HEAD 2> /dev/null ; or command git rev-parse --short HEAD 2> /dev/null)
-
-    if [ "$dirty" != "0" ]
+    if [ $dirty != 0 ]
       set_color -b normal
       set_color red
       echo "$dirty changed file"
-      if [ "$dirty" != "1" ]
+      if [ $dirty != 1 ]
         echo "s"
       end
       echo " "
@@ -19,11 +18,37 @@ function get_git_status -d "Gets the current git status"
       set_color white
     end
 
-    echo " $ref "
+    echo " $branch "
     set_color normal
-   end
+  end
+end
+
+
+function __cmorrell_get_git_status -d "Gets the current git status"
+  if [ -d ".git" ]
+    if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+      set -l dirty (command git status -s --ignore-submodules=dirty | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
+      set -l ref (command git symbolic-ref --short HEAD 2> /dev/null ; or command git rev-parse --short HEAD 2> /dev/null)
+
+      echo $dirty
+      echo $ref
+     end
+  end
+end
+
+function __cmorrell_get_hg_status -d "Gets the current hg status"
+  if [ -d ".hg" ]
+    if command hg id > /dev/null 2>&1
+      set -l dirty (command hg status | wc -l)
+      set -l branch (command hg branch)
+
+      echo $dirty
+      echo $branch
+    end
+  end
 end
 
 function fish_right_prompt -d "Prints right prompt"
-  get_git_status
+  __cmorrell_show_vcs_status (__cmorrell_get_git_status)
+  __cmorrell_show_vcs_status (__cmorrell_get_hg_status)
 end
